1bbe421060276c4b4acd10c2401b52a41d632621
Joona Juusti <joona.juusti@gmail.com>
2018-04-21 21:20:32 +0300

    Add boolean value to check if user is admin

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,6 +3,7 @@ export class User {
     public firstName: string,
     public lastName: string,
     public username: string,
-    public password: string
+    public password: string,
+    public admin: boolean
   ) {}
 }
