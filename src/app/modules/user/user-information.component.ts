ac6f31b8536e74ee5e784676fd3a56dc64d497b3
Joona Juusti <joona.juusti@gmail.com>
2018-05-04 19:27:32 +0300

    Add a form for changing user's information

--- src/app/modules/user/user-information.component.ts
+++ src/app/modules/user/user-information.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+import { AlertService } from '../../services/alert.service';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
 
 @Component({
-  template:`This is the UserInformationComponent.`
+  templateUrl: './user-information.component.html'
 })
 export class UserInformationComponent {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private alertService: AlertService) {}
 
   currentUser: User;
 
   ngOnInit() {
     this.currentUser = this.userService.getCurrentUser();
+  }
+
+  saveInformation(form: NgForm) {
+    if(!this.haveValuesChanged(form)){
+      this.alertService.error('You didn\'t submit any new values!', false);
+    }
+  }
+
+  haveValuesChanged(form : NgForm) {
+    if(this.currentUser.creditCard === form.value.creditCard
+    && this.currentUser.address === form.value.address) {
+      return false;
+    }else return true;
   }
 }
