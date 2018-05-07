a0e16c6a74627c17875cef0d52f6cdfc02f350d0
Niklas Niemelä <niklas.niemela@hotmail.com>
2018-05-07 15:39:24 +0300

    Added default time on route search

--- src/app/modules/admin/route.component.ts
+++ src/app/modules/admin/route.component.ts
@@ -8,11 +8,25 @@ import { AlertService } from '../../services/alert.service';
 @Component({
   templateUrl: './route.component.html'
 })
+
 export class RouteComponent {
   constructor(
     private routeService: RouteService,
     private alertService: AlertService
   ) {}
+
+  ngOnInit() { 
+    //Show current time on route departure and arrival
+    //TO-DO 
+    var v: string;
+    var d = new Date()
+    var month;
+    month = d.getMonth();
+    var day = d.getDate();
+    v=d.getFullYear() +"-0"+ d.getMonth() +"-0"+ d.getDate() +"T"+ d.getHours() +":"+ d.getMinutes();
+    (<HTMLInputElement>document.getElementById("deptTime")).value = v;
+    (<HTMLInputElement>document.getElementById("arriTime")).value = v;
+  }
 
   addRoute(form: NgForm) {
     const wayStations = this.checkWayStations(form.value.wayStations);
@@ -34,4 +48,11 @@ export class RouteComponent {
     else return null;
   }
 
+  currentDate() {
+    var value: string;
+    var d = new Date()
+    value=d.getFullYear +"-"+ d.getMonth +"-"+ d.getDate +"T"+ d.getHours +":"+ d.getMinutes;
+    alert(value);
+    alert(document.getElementById("deptTime").innerHTML) 
+  }
 }
